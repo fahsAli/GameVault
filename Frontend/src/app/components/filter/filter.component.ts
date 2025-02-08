cbc47a5cc798c9dfe9c2db993ab333aed03db1cc
fahsali <ali777fahs@gmail.com>
2025-02-08 22:14:30 +0100

    (Frontend) getting data from filter search

--- Frontend/src/app/components/filter/filter.component.ts
+++ Frontend/src/app/components/filter/filter.component.ts
@@ -1,14 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { GenreService } from '../../services/genre.service';
 import { Genre } from '../../interfaces/genre.interface';
 import { PlatformService } from '../../services/platform.service';
 import { Platform } from '../../interfaces/platform.interface';
 import { RatingService } from '../../services/rating.service';
 import { Rating } from '../../interfaces/rating.interface';
+import { Filter } from '../../interfaces/filter.interface';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
@@ -21,6 +23,29 @@ export class FilterComponent {
 
   private ratingService = inject(RatingService);
   ratings: Rating[] = [];
+
+  gameName: string = "";
+  genre: string = "";
+  platform: string = "";
+  rating: string = "";
+
+  filters : Filter = {
+    name: "",
+    genre: "",
+    platform: "",
+    rating: "",
+  };
+
+  @Output() filterChange = new EventEmitter<Filter>();
+
+  onSearch() {
+    this.filters.name = this.gameName;
+    this.filters.genre = this.genre;
+    this.filters.platform = this.platform;
+    this.filters.rating = this.rating;
+    this.filterChange.emit(this.filters);
+    console.log('Filters applied:', this.filters);
+  }
 
   constructor() {
     this.genreService.getGenres().subscribe({
---
+++ Frontend/src/app/interfaces/filter.interface.ts
@@ -0,0 +1,6 @@
+export interface Filter {
+    name: string | null;
+    genre: string | null;
+    platform: string | null;
+    rating: string | null;
+};
